0d6f7e8d5462ecf49ad239e6e739e61475136b1d
thiago <thiagofreitas1106@gmail.com>
2024-05-16 11:50:21 -0300

    header

---
+++ .history/src/components/Header_20240516114349.jsx
@@ -0,0 +1,39 @@
+import { Box, Button, Typography } from "@mui/material";
+
+const Header = () => {
+  return (
+    <>
+      <Box backgroundColor="#f202" width="100vw" height="50px" display="flex">
+        <Box
+          display="flex"
+          width="1100px"
+          backgroundColor="grey"
+          marginLeft="10px"
+          justifyContent="center"
+          textAlign="center"
+          alignItems="center"
+          gap={10}
+        >
+          <Typography>Sobre nós</Typography>
+          <Typography>Como funciona</Typography>
+          <Typography>Valores</Typography>
+          <Typography>Perguntas</Typography>
+        </Box>
+        <Box display="flex" width="100vw" justifyContent="end" alignItems="center">
+          <Button
+            variant="contained"
+            style={{
+              width: "60px",
+              height: "40px",
+              marginRight: "10px"
+            }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
---
+++ .history/src/components/Header_20240516114551.jsx
@@ -0,0 +1,39 @@
+import { Box, Button, Typography } from "@mui/material";
+
+const Header = () => {
+  return (
+    <>
+      <Box backgroundColor="#20022ed2" width="100vw" height="50px" display="flex">
+        <Box
+          display="flex"
+          width="1100px"
+          marginLeft="10px"
+          justifyContent="center"
+          textAlign="center"
+          alignItems="center"
+          gap={10}
+          color="#ffffff"
+        >
+          <Typography>Sobre nós</Typography>
+          <Typography>Como funciona</Typography>
+          <Typography>Valores</Typography>
+          <Typography>Perguntas</Typography>
+        </Box>
+        <Box display="flex" width="100vw" justifyContent="end" alignItems="center">
+          <Button
+            variant="contained"
+            style={{
+              width: "60px",
+              height: "40px",
+              marginRight: "10px"
+            }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
